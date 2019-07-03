819bb50def0d075c26ed4f1eb58d91f7e75650aa
AnkurYadav619 <yadavankur619@gmail.com>
2019-07-03 09:16:42 +0530

    Update userAsset.php

--- userAsset.php
+++ userAsset.php
@@ -5,17 +5,97 @@
 <head>
 	<title>User</title>
 	<link rel="stylesheet" href="style.css">
+	<script>
+		function assetView(){
+			var a = document.getElementById("assets");
+			var b = document.getElementById("form4");
+
+			if(a.style.display ==="none")
+			{
+				a.style.display = "block";
+				b.style.display = "none";
+			}
+			else
+			{
+				a.style.display = "none";
+			}
+		}
+
+		function assetTransfer(){
+			var a = document.getElementById("assets");
+			var b = document.getElementById("form4");
+			
+  			if (b.style.display === "none") {
+   				 a.style.display = "none";
+   				 b.style.display = "block";
+  			} else {
+   				 b.style.display = "none";
+  			}
+		}
+	</script>
 </head>
 <body>
-	<div id="adminHeader">
+<div class="container">
+	<div class="adminheader">
 		<h2>Asset User: <?php echo $_SESSION['username'];?></h2>
-		<a style="text-decoration: none;float:right;margin-left:15px;" href="logout.php">Logout</a>
-		<a style="text-decoration: none;float:right;" href="index.php">Back to Login</a>
 	</div>
-	 <form id="assetTransferRequest" name="userAssetViewer" action="userAsset.php" method="POST" style="margin-left:400px;margin-right: 220px; ">
- 		<b  style="margin-left:280px;">Transfer Asset</b>
+	<nav>
+			<br>
+			<button id="btn4" type="button" onclick="assetView()">View Asset</button>
+			<button id="btn5" type="button" onclick="assetTransfer()">Request Transfer</button>
+			<a href="index.php">Back to Login</a>
+			<a href="logout.php">Logout</a>
+			
+	</nav>
+<article>
+<div id="assets">
+	<b>Assets</b>
+	<br>
+	<br>
+	<table id="assetViewerTable1" name="assetView" border="1" form="assetViewer">
+			<thead>
+			<tr>
+			   	<th>Sr.No.</th>
+				<th>Item Description</th>
+				<th>Qty.</th>
+				<th>Make</th>
+			</tr>
+		</thead>
+<?php
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "";
+ $db = "assetmanager";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 		$user=$_SESSION['UserNo']; 
+	        $q="Select * from userassets where UserNo=$user";
+	        mysqli_query($conn, $q);
+	        $result = mysqli_query($conn, $q);
+	        $count=mysqli_num_rows($result);
+	        if (mysqli_num_rows($result) > 0) 
+	        {
+			    while($row = mysqli_fetch_assoc($result)) 
+			    {
+			?>
+			        <tr>
+			        	<td>
+			            <?php
+			                echo " " . $row["SerialNo"]. " </td><td>" . $row["Description"]."</td><td> ".$row["Quantity"]."</td><td> ".$row["Make"]." ";
+			            ?>
+			        	</td>
+			    	</tr><?php
+				}
+			 } else 
+			 {}
+ mysqli_close($conn);
+?>
+</table>
+</div>
+<div id="form4">
+	 <form id="assetTransferRequest" name="userAssetViewer" action="userAsset.php" method="POST" >
+ 		<b>Transfer Asset</b>
  		<br>
-
+ 		<br>
 			<label >Transfer To:</label>
 			<select name="userNum3" id="userSelector3" required>
 				<option value="">--Select--</option>
@@ -32,7 +112,7 @@
 			</select>
 			<br>
 			<br>
-		<table id="assetViewerTable" name="assetView" border="1" form="assetViewer" style="width:715px;">
+		<table id="assetViewerTable2" name="assetView" border="1" form="assetViewer">
 			<tr>
 				<th>Mark</th>	
 				<th>Sr.No.</th>
@@ -64,7 +144,6 @@
 			                echo " " . $row["SerialNo"]. " </td><td>" . $row["Description"]."</td><td> ".$row["Quantity"]."</td><td> ".$row["Make"]." ";
 			            ?>
 			        	</td>
-			</select>
 			    	</tr><?php
 				}
 			 } else 
@@ -86,7 +165,10 @@
     mysqli_close($conn);
 ?>
  		</table>
- 		<input class="btn" id="btn7" type="submit" name="request" value="Transfer" style="width:80px;margin-top:5px;"/>
+ 		<input class="btn" id="btn7" type="submit" name="request" value="Request"/>
  	</form>
+ </div>
+ </article>
+ 	</div>
  	</body>
 </HTML>
